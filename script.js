645c83a6df556985994de20686a8c8df2a0c0ef7
Alex Paclin <alex.paclin@gmail.com>
2020-11-11 09:24:59 +0500

    checkRequired

--- script.js
+++ script.js
@@ -4,49 +4,39 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-const showError = (input, message) => {
+function showError(input, message) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control error';
 	const small = formControl.querySelector('small');
 	small.innerText = message;
-};
+}
 
-const showSuccess = (input) => {
+function showSuccess(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
-};
+}
 
-const isValidEmail = (email) => {
+function isValidEmail(email) {
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
-};
+}
+
+function checkRequired(inputArr) {
+	inputArr.forEach((input) => {
+		if (input.value.trim().length === 0) {
+			showError(input, `${getFieldName(input)} is required`);
+		} else {
+			showSuccess(input);
+		}
+	});
+}
+
+function getFieldName(input) {
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	if (username.value.length === 0) {
-		showError(username, 'User name is required');
-	} else {
-		showSuccess(username);
-	}
-
-	if (email.value.length === 0) {
-		showError(email, 'E-mail is required');
-	} else if (!isValidEmail(email.value)) {
-		showError(email, 'E-mail is not valid');
-	} else {
-		showSuccess(email);
-	}
-
-	if (password.value.length === 0) {
-		showError(password, 'Password is required');
-	} else {
-		showSuccess(password);
-	}
-
-	if (password2.value.length === 0) {
-		showError(password2, 'Password confirm is required');
-	} else {
-		showSuccess(password2);
-	}
+	checkRequired([username, email, password, password2]);
 });
